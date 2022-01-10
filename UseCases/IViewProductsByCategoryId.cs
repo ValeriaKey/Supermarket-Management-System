4ffd0e30a75e00b3f055f2da9254c6b9b1eadd66
ValeriaKey <valeriakolossova@outlook.com>
2022-01-10 12:01:55 +0300

    Cashiers Console

---
+++ UseCases/IViewProductsByCategoryId.cs
@@ -0,0 +1,12 @@
+﻿using CoreBusiness;
+using System.Collections.Generic;
+
+namespace UseCases
+{
+    public interface IViewProductsByCategoryId
+    {
+        IEnumerable<Product> Execute(int categoryId);
+
+
+    }
+}
